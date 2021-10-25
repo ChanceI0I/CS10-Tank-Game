06f8ee99009ecad61b2c250cda0d953327faaecd
ChanceI0I <90012159+ChanceI0I@users.noreply.github.com>
2021-10-25 13:23:47 -0600

    Add files via upload

--- tank - Copy/lib.js
+++ tank - Copy/lib.js
@@ -1,58 +1,30 @@
-function key_down(event) {
-    if (event.code == "KeyW") {
-        tank.tank_move_up = true;
-    } else if (event.code == "KeyS") {
-        tank.tank_move_down = true;
-    } else if (event.code == "KeyA") {
-        tank.tank_move_left = true;
-    } else if (event.code == "KeyD") {
-        tank.tank_move_right = true;
-    }
-
-    if (event.code == "KeyJ") {
-        draw_bullet();
-    }
-}
-
-function key_up(event) {
-
-    if (event.code == "KeyW") {
-        tank.tank_move_up = false;
-    } else if (event.code == "KeyS") {
-        tank.tank_move_down = false;
-    } else if (event.code == "KeyA") {
-        tank.tank_move_left = false;
-    } else if (event.code == "KeyD") {
-        tank.tank_move_right = false;
-    }
-}
-
 function drawtank() {
-    ctx.drawImage(tank_img, tank.x, tank.y, tank.size, tank.size);
+    
+    ctx.drawImage(tank.img, tank.x, tank.y, tank.size, tank.size);
 
-    if (tank_move_up) {
-        tank_img = tank.up_img;
-    } else if (tank_move_down) {
-        tank_img = tank.down_img;
-    } else if (tank_move_left) {
-        tank_img = tank.left_img;
-    } else if (tank_move_right) {
-        tank_img = tank.right_img;
+    if (tank.tank_move_up) {
+        tank.img = tank.up_img;
+    } else if (tank.tank_move_down) {
+        tank.img = tank.down_img;
+    } else if (tank.tank_move_left) {
+        tank.img = tank.left_img;
+    } else if (tank.tank_move_right) {
+        tank.img = tank.right_img;
     }
     
-    if (tank_move_up) {
+    if (tank.tank_move_up) {
         if (tank.y > 0) {
             tank.y -= tank.speed;
         }
-    } else if (tank_move_down) {
+    } else if (tank.tank_move_down) {
         if (tank.y + tank.size < canvas.height) {
             tank.y += tank.speed;
         }
-    } else if (tank_move_right) {
+    } else if (tank.tank_move_right) {
         if (tank.x + tank.size < canvas.width) {
-            tank.x += tank_speed;
+            tank.x += tank.speed;
         }
-    } else if (tank_move_left) {
+    } else if (tank.tank_move_left) {
         if (tank.x > 0) {
             tank.x -= tank.speed;
         }
@@ -73,32 +45,81 @@ function locate_muzzle() {
        tank.muzzle_x = tank.x + tank.size;
        tank.muzzle_y = tank.y + tank.size/2 - 2;
     }
-
 }
 
 function draw_bullet() {
-    
-    //make sure the direction of the bullet is correct
-    if (tank.img === tank.up_img || tank.img === tank.down_img) {
-        bullet.bullet_img = bullet.up_down_img;
-    } else if (tank.img === tank.left_img || tank.img === tank.right_img) {
-        bullet.bullet_img = bullet.left_right_img;
-    }
+    // console.log("fire")
 
-    //locate where this muzzle is, and let the bullet shoot from the muzzle
-    locate_muzzle()
+  
+
+    // console.log(tank.img)
+    // console.log(tank.muzzle_x, tank.muzzle_y)
+    // console.log(bullet.x, bullet.y, bullet.speed)
+    // console.log(bullet.width, bullet.length)
+
+    // if (tank.img === tank.up_img) {
+    //     function bullet_move_up() {
+    //         ctx.fillStyle = bullet.color;
+    //         ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.length);
+    //         bullet.y -= bullet.speed
+    //         requestAnimationFrame(bullet_move_up);
+    //     }
+    //     requestAnimationFrame(bullet_move_up);
+    // }
     
-    bullet.y = tank.muzzle_y;
-    bullet.x = tank.muzzle_x;
-    console.clear()
+
+    
     if (tank.img === tank.up_img) {
-        for (let i = 0; i < canvas.height; i += bullet.speed) {
-            ctx.drawImage(bullet.bullet_img, bullet.x, bullet.y, bullet.width, bullet.length);
+        console.log("up")
+        // ctx.moveTo(tank.muzzle_x, tank.muzzle_y)
+        bullet.x = tank.muzzle_x;
+        bullet.y = tank.muzzle_y;
+        function bullet_move_up() {
+            ctx.fillStyle = bullet.color;
+            ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.length);
             bullet.y -= bullet.speed;
-            
-            console.log(bullet.y, bullet.x)
+            requestAnimationFrame(bullet_move_up);
         }
+        requestAnimationFrame(bullet_move_up);
+    } else if (tank.img === tank.down_img) {
+            console.log("down")
+            // ctx.moveTo(tank.muzzle_x, tank.muzzle_y)
+            bullet.x = tank.muzzle_x;
+            bullet.y = tank.muzzle_y;
+            function bullet_move_down() {
+                ctx.fillStyle = bullet.color;
+                ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.length);
+                bullet.y += bullet.speed;
+            requestAnimationFrame(bullet_move_down);
+        }
+        requestAnimationFrame(bullet_move_down);
+    } else if (tank.img === tank.left_img) {
+            console.log("left")
+            // ctx.moveTo(tank.muzzle_x, tank.muzzle_y)
+            bullet.x = tank.muzzle_x;
+            bullet.y = tank.muzzle_y;
+            function bullet_move_left() {
+                ctx.fillStyle = bullet.color;
+                ctx.fillRect(bullet.x, bullet.y, bullet.length, bullet.width);
+                bullet.x -= bullet.speed;
+            requestAnimationFrame(bullet_move_left);
+        }
+        requestAnimationFrame(bullet_move_left);
+    } else if (tank.img === tank.right_img) {
+            console.log("right")
+            // ctx.moveTo(tank.muzzle_x, tank.muzzle_y)
+            bullet.x = tank.muzzle_x;
+            bullet.y = tank.muzzle_y;
+            function bullet_move_right() {
+                ctx.fillStyle = bullet.color;
+                ctx.fillRect(bullet.x, bullet.y, bullet.length, bullet.width);
+                bullet.x += bullet.speed;
+        requestAnimationFrame(bullet_move_right);
     }
+    requestAnimationFrame(bullet_move_right);
+}
+
+
 }
 
 const tank = {
@@ -122,14 +143,11 @@ const tank = {
 }
 
 const bullet = {
-    up_down_img : document.getElementById("bullet_up_down"),
-    left_right_img : document.getElementById("bullet_left_right"),
-    bullet_img : document.getElementById("bullet_up_down"),
-
-    length : 5,
+    
+    length : 7,
     width : 2,
-    color : "black",
-    speed : 2,
+    color : "red",
+    speed : 3,
     fire : false,
 
     x : tank.muzzle_x,
--- tank - Copy/main.js
+++ tank - Copy/main.js
@@ -1,51 +1,42 @@
 let canvas = document.getElementById("draw");
 let ctx = canvas.getContext("2d");
-// let tank_up = document.getElementById("tank_up");
-// let tank = tank_up;
-// let tank_down = document.getElementById("tank_down");
-// let tank_left = document.getElementById("tank_left");
-// let tank_right = document.getElementById("tank_right");
-// let tank_x = 20;
-// let tank_y = 20;
-// let tank_move_up = false;
-// let tank_move_down = false;
-// let tank_move_right = false;
-// let tank_move_left = false;
-// let tank_speed = 2;
-// let tank_size = 35;
-
-// let bullet_size = 15;
-// let bullet_up_down = document.getElementById("bullet_up_down");
-// let bullet_left_right = document.getElementById("bullet_left_right");
-// let fire = false;
-// let bullet;
-// let muzzle_x;
-// let muzzle_y;
-// let bullet_y;
-// let bullet_x;
-// let bullet_width = 2;
-// let bullet_height = 5;
-// let bullet_speed = 0.1;
-
-// let enemy_target = document.getElementById("enemy_target");
-// let enemy_explode = document.getElementById("enemy_explode");
-// let enemy_x = Math.random() * 1000;
-// let enemy_y = Math.random() * 1000;
-// let enemy_size = 30;
-// let enemy_pic = enemy_target;
-// let enemy_dead = false;
 
 document.addEventListener("keydown", key_down, false);
 document.addEventListener("keyup", key_up, false);
 
-key_down(event);
-key_up(event);
-drawtank();
-locate_muzzle(); 
-draw_bullet();
+function key_down(event) {
+    if (event.code == "KeyW") {
+        tank.tank_move_up = true;
+    } else if (event.code == "KeyS") {
+        tank.tank_move_down = true;
+    } else if (event.code == "KeyA") {
+        tank.tank_move_left = true;
+    } else if (event.code == "KeyD") {
+        tank.tank_move_right = true;
+    }
+
+    if (event.code == "KeyJ") {
+        draw_bullet();
+    }
+}
+
+function key_up(event) {
+
+    if (event.code == "KeyW") {
+        tank.tank_move_up = false;
+    } else if (event.code == "KeyS") {
+        tank.tank_move_down = false;
+    } else if (event.code == "KeyA") {
+        tank.tank_move_left = false;
+    } else if (event.code == "KeyD") {
+        tank.tank_move_right = false;
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawtank();
+    locate_muzzle();
+    requestAnimationFrame(draw);
 }
-setInterval(draw, 10);
+requestAnimationFrame(draw);
